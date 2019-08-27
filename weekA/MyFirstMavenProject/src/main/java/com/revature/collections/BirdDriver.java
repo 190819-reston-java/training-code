fe3624ad82b63ab043766d9c90937b04baa67a38
AdamDKing <adamdentonking@gmail.com>
2019-08-27 11:35:32 -0400

    continued Bird example

---
+++ weekA/MyFirstMavenProject/src/main/java/com/revature/collections/BirdDriver.java
@@ -0,0 +1,16 @@
+package com.revature.collections;
+
+public class BirdDriver {
+
+	public static void main(String[] args) {
+		System.out.println(new Sparrow());
+		
+		Buzzard.printPopulation();
+		
+		System.out.println(new Buzzard());
+		
+		Buzzard.printPopulation();
+		
+	}
+
+}
--- weekA/MyFirstMavenProject/src/main/java/com/revature/collections/Sparrow.java
+++ weekA/MyFirstMavenProject/src/main/java/com/revature/collections/Sparrow.java
@@ -3,41 +3,60 @@ package com.revature.collections;
 import java.io.Serializable;
 
 public class Sparrow extends Bird implements Passering, Serializable {
+	//start off with no Sparrows
+	public static int population = 0;
 
 	public Sparrow(String name, String color, boolean feathered) {
 		super(name, color, feathered);
-		// TODO Auto-generated constructor stub
+		population++;
 	}
 
 	public Sparrow(String name, String color) {
 		super(name, color);
-		// TODO Auto-generated constructor stub
+		population++;
 	}
 
 	public Sparrow(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
+		population++;
 	}
 
 	public Sparrow() {
-		// TODO Auto-generated constructor stub
+		population++;
 	}
 
 	public void perch() {
-		// TODO Auto-generated method stub
+		System.out.println(this.getName() + " is perching!");
 
 	}
-
 	@Override
 	public void sitStill() {
-		// TODO Auto-generated method stub
+		perch();
 
 	}
 
 	@Override
 	public void tryEat() {
-		// TODO Auto-generated method stub
-
+		System.out.println("Cannot eat nothing :(");
+	}
+	
+	public void tryEat(Worm w) {
+		System.out.println(this.getName() + " is eating a worm");
+	}
+	
+	public void tryEat(Carrion c) {
+		System.out.println(this.getName() + " can't eat carrion");
+	}
+	
+	//since this overloaded method is more specific than the varargs method
+	// it will be called when theres only one int
+	public void tryEat(int i) {
+		System.out.println(this.getName() + " can't eat an int");
+	}
+	
+	//varargs takes any number of ints
+	public void tryEat(int... ints) {
+		System.out.println(this.getName() + " can't eat ints");
 	}
 
 }

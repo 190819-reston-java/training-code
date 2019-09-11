9d12d25e73b030372f4c1f818511b630f9ae250c
AdamDKing <adamdentonking@gmail.com>
2019-09-11 14:39:53 -0400

    modified driver slightly

--- week2/jdbcExample/src/main/java/com/revature/Driver.java
+++ week2/jdbcExample/src/main/java/com/revature/Driver.java
@@ -1,6 +1,7 @@
 package com.revature;
 
 import com.revature.model.Player;
+import java.time.Instant;
 import com.revature.repositories.PlayerDAO;
 import com.revature.repositories.PlayerDAOImplPJDBC;
 import com.revature.utils.ConnectionUtil;
@@ -15,14 +16,20 @@ public class Driver {
 		playerDAO.getPlayers()
 				.forEach((Player p)->{System.out.println(p);});
 		
-		playerDAO.createPlayer(new Player(0L, "Sammy Sosa", 21L, "RF", 0.4));
+		//playerDAO.createPlayer(new Player(0L, "Sammy Sosa", 21L, "RF", 0.4));
 		
 		for(Player p : playerDAO.getPlayers()) {
+			if (p.getName().equals("Test name 1")) {
+				p.setName("Mark Mcgwire");
+				playerDAO.updatePlayer(p);
+			}
 			System.out.println(p);
 		}
 		
 		System.out.println(playerDAO.getPlayer("Test name 1"));
 		System.out.println(playerDAO.getPlayer(4L));
+		
+		
 
 	}
 

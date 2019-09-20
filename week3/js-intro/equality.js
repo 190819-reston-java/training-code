f0226d73d5e44f4eaf72efd96c4fe2561d187e32
AdamDKing <adamdentonking@gmail.com>
2019-09-20 02:46:12 -0400

    add equality examples and comments

--- week3/js-intro/equality.js
+++ week3/js-intro/equality.js
@@ -2,6 +2,12 @@
 
 console.log('hello from equality.js!');
 
+//There are two types of equals in js:
+// double equals : ==
+//      compares value without comparing type
+// triple equals : ===
+//      compares value and type
+
 //define a function compare
 function compare(a, b) {
     console.log('Comparing ' + a + ' and ' + b + ':');
@@ -14,6 +20,12 @@ function compare(a, b) {
     console.log(`a===b : ${a===b}`);
 }
 
-//Punchline: always use ===
+compare(45, "blue");
+compare(45, "45");
+compare(1, 1);
+compare("", 0);
+compare(1, 1.0);
+compare(0, {});
+compare(null, {});
 
-//TODO: add examples in script
+//Punchline: always use ===

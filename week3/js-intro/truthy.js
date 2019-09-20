55d749800499740bfd615fcaf657f4275078ee62
AdamDKing <adamdentonking@gmail.com>
2019-09-20 02:47:48 -0400

    add truthy examples and comments

--- week3/js-intro/truthy.js
+++ week3/js-intro/truthy.js
@@ -5,3 +5,28 @@ function convertToBool(x) {
     console.log(`${x} as boolean: ${Boolean(x)}`);
 }
 
+convertToBool(3 === 2);
+convertToBool(1);
+convertToBool(null);
+convertToBool(undefined);
+convertToBool(30);
+convertToBool(-2);
+convertToBool("");
+convertToBool("adam");
+convertToBool("   ");
+convertToBool(Infinity/-Infinity);
+convertToBool({});
+convertToBool([]);
+convertToBool(NaN);
+convertToBool({ name:"myObj" });
+
+//falsy values:
+//null
+//undefined
+//""
+//false
+//NaN
+//0
+//-0 (dont use this)
+
+//everything else is truthy

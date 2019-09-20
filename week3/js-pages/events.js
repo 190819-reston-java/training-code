fd6d52b52b45ea96a860b60577d6bf8b9ca9fc50
AdamDKing <adamdentonking@gmail.com>
2019-09-20 01:41:13 -0400

    events continued

--- week3/js-pages/events.js
+++ week3/js-pages/events.js
@@ -4,16 +4,29 @@ let innermost = document.getElementById("innermost");
 let inner = document.querySelector("#inner");
 let outer = document.querySelector("#outer");
 
-let alertClick = (message)=>{
-    return (event)=>{alert(message);};
+let alertClick = (message) => {
+    return (event) => { alert(message); };
 };
 
-innermost.addEventListener("click", (event)=>{
-    alert("Innermost was clicked");
-    event.stopPropagation();
-});
+// innermost.addEventListener("click", (event) => {
+//     alert("Innermost was clicked : bubble");
+//     event.stopPropagation();
+// });
 
-inner.addEventListener("click", alertClick("Inner was clicked"));
+innermost.addEventListener("click",
+    alertClick("Innermost was clicked : bubble"));
 
-outer.addEventListener("click", alertClick("Outer was clicked"));
+inner.addEventListener("click",
+    alertClick("Inner was clicked : bubble"));
 
+outer.addEventListener("click",
+    alertClick("Outer was clicked : bubble"));
+
+inner.addEventListener("click",
+    alertClick("Inner was clicked : capture"), true);
+
+outer.addEventListener("click",
+    alertClick("Outer was clicked : capture"), true);
+
+innermost.addEventListener("click",
+    alertClick("Innermost was clicked : capture"), true);

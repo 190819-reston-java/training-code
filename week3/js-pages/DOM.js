ad3dcce37c99ee7aa1a04ddfe2664b2d20b7e94c
AdamDKing <adamdentonking@gmail.com>
2019-09-20 10:06:16 -0400

    finish DOM manipulation page

--- week3/js-pages/DOM.js
+++ week3/js-pages/DOM.js
@@ -8,20 +8,37 @@ let demoDiv = document.querySelector(".dom-demo");
 let button = document.querySelector(".btn");
 
 let clickerCounter = 0;
-
-// Most DOM manipulation happens in callbacks:
-button.addEventListener("click", ()=>{
-  clickerCounter++;
-  text.innerText = `Button has been clicked ${clickerCounter} times`;
-});
+let hasBeenClicked = false;
 
 // Make elements using document
 let newButton = document.createElement("button");
 newButton.innerText = "Reset Counter";
-newButton.addEventListener("click", ()=>{
+// array with single string: btn
+// sets the class to btn to use Bootstrap button:
+newButton.classList = ["btn"];
+newButton.addEventListener("click", () => {
   clickerCounter = 0;
   text.innerText = "Counter was reset";
 });
 
-// Need to append new elements for them to appear
-demoDiv.appendChild(newButton);
+// Most DOM manipulation happens in callbacks:
+button.addEventListener("click", () => {
+  clickerCounter++;
+  text.innerText = `Button has been clicked ${clickerCounter} times`;
+
+  if(!hasBeenClicked) {
+    hasBeenClicked = true;
+    // Need to append new elements for them to appear
+    demoDiv.appendChild(newButton);
+  }
+});
+
+let toggle = false;
+demoDiv.addEventListener("mouseover", () => {
+  toggle = !toggle;
+  if (toggle) {
+    demoDiv.style.backgroundColor = "white";
+  } else {
+    demoDiv.style.backgroundColor = "blanchedalmond";
+  }
+});

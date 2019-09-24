4916decde784a3a1e7ce0e285e6987ca48425d6d
AdamDKing <adamdentonking@gmail.com>
2019-09-24 10:52:34 -0400

    hellow world Servlet example

---
+++ week4/helloServlets/src/main/java/com/revature/hello/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.revature.hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	
+	//not the right way to do things, we'll use session storage
+	// or similar in future examples
+	static int count = 0;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Handling a GET in HelloServlet");
+		
+		//when we write to the response, we use a PrintWriter
+		PrintWriter pw = resp.getWriter();
+		//whatever we write using pw will go in the body of the response
+		pw.write("Writing to response from HelloServlet."
+				+ " Has happened " + ++count + " times");
+		//super.doGet(req, resp);
+	}
+
+}

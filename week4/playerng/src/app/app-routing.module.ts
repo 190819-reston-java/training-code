bff04bcf6ef1bea0900e3174b05054044a50273b
AdamDKing <adamdentonking@gmail.com>
2019-09-27 10:32:13 -0400

    navbar basics

--- week4/playerng/src/app/app-routing.module.ts
+++ week4/playerng/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SecretPageComponent } from './secret-page/secret-page.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "about",
+    component: AboutComponent,
+  },
+  {
+    path: "secret",
+    component: SecretPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

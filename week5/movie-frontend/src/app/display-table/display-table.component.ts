a84b3485ee53365eeb5be652b54a2108112a7783
AdamDKing <adamdentonking@gmail.com>
2019-10-01 15:28:23 -0400

    general table functionality

--- week5/movie-frontend/src/app/display-table/display-table.component.ts
+++ week5/movie-frontend/src/app/display-table/display-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieHttpService } from '../movie-http.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-display-table',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayTableComponent implements OnInit {
 
-  constructor() { }
+  objects : any = [
+    {"name":"default", "num":3},
+    {"name":"adam", "num":4}
+    ];
+  templateKeys : any = Object.keys(this.objects[0]);
+
+  constructor(private movieHttp : MovieHttpService, private auth : AuthService) { }
 
   ngOnInit() {
+    this.movieHttp.getMovies().subscribe(
+      (data)=>{
+        this.templateKeys = Object.keys(data[0]);
+        this.objects = data;
+      },
+      (error)=>{
+        console.error(error);
+      }
+    )
   }
 
 }
--- week5/movie-frontend/src/app/movie-http.service.ts
+++ week5/movie-frontend/src/app/movie-http.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieHttpService {
 
-  constructor() { }
+  private MOVIES_URL = "https://my-json-server.typicode.com/AdamRev/moviejson/movies";
+  private ACTORS_URL = "https://my-json-server.typicode.com/AdamRev/moviejson/actors";
+  private DIRECTORS_URL = "https://my-json-server.typicode.com/AdamRev/moviejson/directors";
+
+  constructor(private http : HttpClient) { }
+
+  getMovies() {
+    return this.http.get(this.MOVIES_URL);
+  }
+
+  getActors() {
+    return this.http.get(this.ACTORS_URL);
+  }
+
+  getDirectors() {
+    return this.http.get(this.DIRECTORS_URL);
+  }
+
 }

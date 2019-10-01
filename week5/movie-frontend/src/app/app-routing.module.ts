26d8e3253b081ce2df89cd0864281e877ebed7fe
AdamDKing <adamdentonking@gmail.com>
2019-10-01 13:57:39 -0400

    footer

--- week5/movie-frontend/src/app/app-routing.module.ts
+++ week5/movie-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesDashComponent } from './movies-dash/movies-dash.component';
+import { ActorsDashComponent } from './actors-dash/actors-dash.component';
+import { DirectorsDashComponent } from './directors-dash/directors-dash.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "movies",
+    component : MoviesDashComponent,
+  },
+  {
+    path : "actors",
+    component : ActorsDashComponent,
+  },
+  {
+    path : "directors",
+    component : DirectorsDashComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

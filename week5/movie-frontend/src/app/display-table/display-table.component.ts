980fa81e6fcff7dcb6f0674d5f05d231b3b2e7ee
AdamDKing <adamdentonking@gmail.com>
2019-10-01 15:58:09 -0400

    table now takes arbitrary input

--- week5/movie-frontend/src/app/display-table/display-table.component.ts
+++ week5/movie-frontend/src/app/display-table/display-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { MovieHttpService } from '../movie-http.service';
 import { AuthService } from '../auth.service';
 
@@ -7,26 +7,23 @@ import { AuthService } from '../auth.service';
   templateUrl: './display-table.component.html',
   styleUrls: ['./display-table.component.css']
 })
-export class DisplayTableComponent implements OnInit {
+export class DisplayTableComponent implements OnInit, OnChanges {
 
+  @Input("objects")
   objects : any = [
     {"name":"default", "num":3},
     {"name":"adam", "num":4}
     ];
-  templateKeys : any = Object.keys(this.objects[0]);
+  templateKeys : any;
 
-  constructor(private movieHttp : MovieHttpService, private auth : AuthService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.movieHttp.getMovies().subscribe(
-      (data)=>{
-        this.templateKeys = Object.keys(data[0]);
-        this.objects = data;
-      },
-      (error)=>{
-        console.error(error);
-      }
-    )
+    
+  }
+
+  ngOnChanges() {
+    this.templateKeys = Object.keys(this.objects[0]);
   }
 
 }
--- week5/movie-frontend/src/app/movies-dash/movies-dash.component.ts
+++ week5/movie-frontend/src/app/movies-dash/movies-dash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieHttpService } from '../movie-http.service';
 
 @Component({
   selector: 'app-movies-dash',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesDashComponent implements OnInit {
 
-  constructor() { }
+  movieList : any = [{"title":3},{"title":5}];
+
+  constructor(private movieHttp :MovieHttpService) { }
 
   ngOnInit() {
+    this.movieHttp.getMovies().subscribe(
+      (data)=>{
+        this.movieList = data;
+      },
+      (error)=>{
+        console.error(error);
+      }
+    );
   }
 
 }

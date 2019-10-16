dedc3945fb0fbed8d0374af961f6fc352a885918
AdamDKing <adamdentonking@gmail.com>
2019-10-16 09:50:15 -0400

    return appcat url method

--- week7/discoverer/src/main/java/com/example/demo/DiscovererApplication.java
+++ week7/discoverer/src/main/java/com/example/demo/DiscovererApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,11 @@ public class DiscovererApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscovererApplication.class, args);
+	}
+	
+	@GetMapping("/appcaturl")
+	public String getAppcatUrl() {
+		return ec.getNextServerFromEureka("appcat", false).getHomePageUrl();
 	}
 
 }
